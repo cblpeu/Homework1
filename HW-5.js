2ead3dafe7022d24ad49f00698fed79ea37e3c7c
cblpeu <sofakirk@yandex.ru>
2024-04-23 22:48:36 +0300

    modified:   HW-5.js

--- HW-5.js
+++ HW-5.js
@@ -78,3 +78,19 @@ const circle2 = {
     getPerimeter,
 };
 
+
+//Задание 8
+function season(month) {
+    if (month >= 3 && month <= 5) {
+        return "Весна";
+    } else if (month >= 6 && month <= 8) {
+        return "Лето";
+    } else if (month >= 9 && month <= 11) {
+        return "Осень";
+    } else if (month == 1 || month == 2 || month == 12 ) {
+        return "Зима";
+    } else {
+        return "Что-то странное)"
+    }
+}
+console.log(season(prompt("Введите число")));
